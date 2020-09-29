ae67dc9f0a610f7162e985147f2a3c7ce185ed13
Willits-Alec <alecwillits@gmail.com>
2020-09-29 14:17:21 -0600

    Add project files.

---
+++ MegaDesk-Willits/MainMenue.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MegaDesk_Willits
+{
+    partial class MegaDesk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddNewQuote = new System.Windows.Forms.Button();
+            this.ViewQuotes = new System.Windows.Forms.Button();
+            this.SearchQuotes = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AddNewQuote
+            // 
+            this.AddNewQuote.Location = new System.Drawing.Point(12, 12);
+            this.AddNewQuote.Name = "AddNewQuote";
+            this.AddNewQuote.Size = new System.Drawing.Size(406, 81);
+            this.AddNewQuote.TabIndex = 0;
+            this.AddNewQuote.Text = "Add New Quote";
+            this.AddNewQuote.UseVisualStyleBackColor = true;
+            // 
+            // ViewQuotes
+            // 
+            this.ViewQuotes.Location = new System.Drawing.Point(12, 110);
+            this.ViewQuotes.Name = "ViewQuotes";
+            this.ViewQuotes.Size = new System.Drawing.Size(406, 81);
+            this.ViewQuotes.TabIndex = 1;
+            this.ViewQuotes.Text = "View Quotes";
+            this.ViewQuotes.UseVisualStyleBackColor = true;
+            // 
+            // SearchQuotes
+            // 
+            this.SearchQuotes.Location = new System.Drawing.Point(12, 208);
+            this.SearchQuotes.Name = "SearchQuotes";
+            this.SearchQuotes.Size = new System.Drawing.Size(406, 81);
+            this.SearchQuotes.TabIndex = 2;
+            this.SearchQuotes.Text = "Search Quotes";
+            this.SearchQuotes.UseVisualStyleBackColor = true;
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(12, 306);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(406, 81);
+            this.Exit.TabIndex = 3;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            // 
+            // MegaDesk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 579);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.SearchQuotes);
+            this.Controls.Add(this.ViewQuotes);
+            this.Controls.Add(this.AddNewQuote);
+            this.Name = "MegaDesk";
+            this.Text = "MegaDesk";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddNewQuote;
+        private System.Windows.Forms.Button ViewQuotes;
+        private System.Windows.Forms.Button SearchQuotes;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
